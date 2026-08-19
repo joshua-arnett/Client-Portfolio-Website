32d3f987fb44d7d1ac3b491de1cd57c7410b70fd
Joshua Arnett <joshua.arnett2004@gmail.com>
2026-08-19 17:26:36 +0700

    Applied style changes. Simplified webpage code and layout.

--- React App/src/App.jsx
+++ React App/src/App.jsx
@@ -1,29 +1,27 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
+import TopBar from './components/TopBar';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import WorksPage from './pages/WorksPage';
 import Footer from './components/Footer'
 import './styles.css';
 
-// Main app layout: sidebar on the left, page content in the middle, footer below.
+// Main app layout:  Top bar on top, page content in the middle, footer below.
 export default function App() {
   return (
     <div className="app-container">
-      <Sidebar />
-      <div className="app-shell">
-        <main className="main-page">
-          <Routes>
-            {/* Redirect the root URL to the home page */}
-            <Route path="/" element={<Navigate to="/home" replace />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/works" element={<WorksPage />} />
-            <Route path="/works/:slug" element={<WorksPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <TopBar />
+      <main className="main-page">
+        <Routes>
+          {/* Redirect the root URL to the home page */}
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/works" element={<WorksPage />} />
+          <Route path="/works/:slug" element={<WorksPage />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
